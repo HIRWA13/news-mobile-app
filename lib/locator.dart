b77c05fe186e22e8445d590daad603e464982366
Junior Hirwa <iamhirwejr@gmail.com>
2024-12-13 16:13:43 +0200

    chore:  register instances

---
+++ lib/locator.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+import 'package:news_app/features/news_page/data/news_data_source/article_data_source.dart';
+import 'package:news_app/features/news_page/data/news_repositories/remote/remote_article_repository.dart';
+
+import 'features/news_page/domain/news_use_cases/article_use_cases.dart';
+
+final GetIt locator = GetIt.instance;
+
+void initLocator() {
+  locator.registerFactory<ArticleDataSource>(() => ArticleDataSource());
+  locator.registerSingleton<RemoteArticleRepository>(
+      RemoteArticleRepository(locator<ArticleDataSource>()));
+  locator.registerSingleton<ArticleUseCases>(
+      ArticleUseCases(locator<RemoteArticleRepository>()));
+}
