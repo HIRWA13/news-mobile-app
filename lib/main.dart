1cc7ae222c003d5bbcdb6e9ccb23388b6ec5cdc0
Junior Hirwa <iamhirwejr@gmail.com>
2024-12-16 21:28:11 +0200

    chore: add font

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter News',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Telegraf'),
       home: Container(
         color: Colors.white,
         child: const Padding(
